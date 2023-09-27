9835fe2b563e624e8c8f13b82f2a880ffacfee67
Santiago Dorado <santiagodoradogomez@gmail.com>
2023-09-27 15:06:41 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import SignInSide from './pages/SignInSide';
+import Blogs from './pages/Blog';
+import Root from './routes/root'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <SignInSide />,
+    },
+    {
+      path: "/Blog",
+      element: <Blogs />
+    }
+  ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
